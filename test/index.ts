df65678b321802a3846d30e0814d42e4192a6fa0
Anton Krug <antonkrug@gmail.com>
2018-04-29 23:47:49 +0100

    Testing reporter

--- test/index.ts
+++ test/index.ts
@@ -3,8 +3,10 @@ import testMochaRunner = require("vscode/lib/testrunner");
 // https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
 
 testMochaRunner.configure({
+    // reporter: "list",
     ui: "tdd",
     useColors: true,
 });
+
 
 module.exports = testMochaRunner;
