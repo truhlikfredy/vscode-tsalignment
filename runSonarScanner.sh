5c2bbafaf7163272a60fba7d0899b3ca27fbac61
Anton Krug <antonkrug@gmail.com>
2018-04-30 00:11:36 +0100

    adding sonarcloud.io scanning

---
+++ runSonarScanner.sh
@@ -0,0 +1,7 @@
+sonar-scanner \
+  -Dsonar.projectKey=ts-align \
+  -Dsonar.organization=truhlikfredy-github \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=$SONAR_VSALIGN_TOKEN
+
